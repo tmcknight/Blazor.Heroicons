4a4835006fd747e1c5b3d58ba64ce24d14e50c58
tmcknight <tom@tmcknight.dev>
2023-12-18 12:51:37 -0500

    Add HeroiconStyle enum

--- src/Blazor.Heroicons/HeroiconBase.cs
+++ src/Blazor.Heroicons/HeroiconBase.cs
@@ -9,27 +9,16 @@ public abstract class HeroiconBase : ComponentBase
     /// </summary>
     [Parameter(CaptureUnmatchedValues = true)]
     public Dictionary<string, object> AdditionalAttributes { get; set; } = new();
-}
 
-/// <summary>
-/// The type/size of the icon.
-/// </summary>
-public enum HeroiconType
-{
-    /// <summary>
-    /// For primary navigation and marketing sections, with a filled appearance.
-    /// </summary>
-    Solid,
-    /// <summary>
-    /// For primary navigation and marketing sections, with an outlined appearance.
-    /// </summary>
-    Outline,
-    /// <summary>
-    /// For smaller elements like buttons, form elements, and to support text.
-    /// </summary>
-    Mini,
-    /// <summary>
-    /// For tighter, higher density UIs.
-    /// </summary>
-    Micro
+    protected static HeroiconStyle GetStyleFromType(HeroiconType? type)
+    {
+        return type switch
+        {
+            HeroiconType.Outline => HeroiconStyle.Outline,
+            HeroiconType.Solid => HeroiconStyle.Solid,
+            HeroiconType.Mini => HeroiconStyle.Mini,
+            HeroiconType.Micro => HeroiconStyle.Micro,
+            _ => HeroiconStyle.Outline
+        };
+    }
 }
---
+++ src/Blazor.Heroicons/HeroiconStyle.cs
@@ -0,0 +1,24 @@
+namespace Blazor.Heroicons;
+
+/// <summary>
+/// The type/size of the icon.
+/// </summary>
+public enum HeroiconStyle
+{
+    /// <summary>
+    /// For primary navigation and marketing sections, with a filled appearance.
+    /// </summary>
+    Solid,
+    /// <summary>
+    /// For primary navigation and marketing sections, with an outlined appearance.
+    /// </summary>
+    Outline,
+    /// <summary>
+    /// For smaller elements like buttons, form elements, and to support text.
+    /// </summary>
+    Mini,
+    /// <summary>
+    /// For tighter, higher density UIs.
+    /// </summary>
+    Micro
+}
